b66c39b87d85053584b85f1e24fa7a6b2df907f5
edimanri <62841454+edimanri@users.noreply.github.com>
2026-09-22 22:18:51 +0000

    fix: Habilitar SSR y crear rutas de API para Keystatic en Vercel

--- src/pages/api/keystatic/[...params].ts
+++ src/pages/api/keystatic/[...params].ts
@@ -1,5 +1,5 @@
 import { makePage } from '@keystatic/astro/api';
-import keystaticConfig from '../../../../keystatic.config';
+import keystaticConfig from '../../../keystatic.config'; // <- Corrección aquí
 
 export const all = makePage(keystaticConfig);
 export const prerender = false;
