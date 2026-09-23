02ec54017ab5892ac02ff68348bbfb008de8a57c
edimanri <62841454+edimanri@users.noreply.github.com>
2026-09-23 11:08:06 +0000

    fix: eliminar rutas duplicadas de keystatic para evitar colision en Vercel

--- src/pages/api/keystatic/[...params].ts
+++
@@ -1,5 +0,0 @@
-import { makePage } from '@keystatic/astro/api';
-import keystaticConfig from '../../../keystatic.config'; // <- Corrección aquí
-
-export const all = makePage(keystaticConfig);
-export const prerender = false;
